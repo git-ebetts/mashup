f9ff43801313333a79edef35e8826e6b42eadb9a
git-ebetts <ebettsmail@gmail.com>
2015-11-05 17:10:09 -0500

    project at V2

--- README.md
+++ README.md
@@ -1,43 +1,11 @@
-# Mashup project
+# Mashup project, V2
 
-This project is open-ended! Requirements:
+A work in progress, Artifacts By Origin intends to collect historic items from various museum collections that originate from the same location.
 
-* Build a site that uses data from at least one external API in an interesting, interactive way.
-* Replace this README with a description of the project.
-* You are welcome to use any 3rd-party libraries/frameworks – just load them from a CDN (e.g. [cdnjs](http://cdnjs.com)), or put them under the [`vendor/`](vendor/) folder.
+On page load, the area presented is Egypt's famed "Valley of the Kings." Clicking on any of the marked historic sites returns a list of ancient items originating at that site. These items are currently in the collections of the Metropolitan Museum of Art (www.metmuseum.org). Clicking on the object's title will take you to the Met's official description and a photo of the object.
 
-The JS code should be **non-trivial**. That being said... start simple! (Just get the data to show up on the page.) No server-side coding is required, but feel free to create a backend in whatever language if you like, if you need one.
+The search can involve any historic site. Refocusing the map to another geographic area and clicking a historic site will yield search results of items in the Metropolitan Museum of Art from that site.
 
-* [AJAX demos](https://github.com/advanced-js/deck/tree/gh-pages/demos/ajax)
-* [inspiration?](http://www.programmableweb.com/mashups)
+The next iteration of this project aims to add items from the British Museum to the search results.
 
-## Finding an API
-
-A couple things to look for in an API (or at least the endpoints you're using) for this project:
-
-* Make sure it doesn't require authentication (e.g. [OAuth](http://oauth.net/)).
-    * You need a server to do this.
-* If the API doesn't support cross-domain requests (JSONP or CORS), you will need to use [JSONProxy](https://jsonp.afeld.me/).
-
-Here is a [list of API suggestions](https://gist.github.com/afeld/4952991).
-
-## V1
-
-Get the data to show up on the page.
-
-## V2
-
-First pass. Get your main feature working in the simplest way possible.
-
-## V3
-
-Iterate!
-
-* Refactor
-* Add a new feature
-
-## Bonus points
-
-* automated tests
-    * [Sinon.js fakeServer](http://sinonjs.org/docs/#fakeServer) may be helpful
-* fancy interactivity/animations
+By linking the digital resources of various museums by a location search, this project intends to reunite artifacts with a common origin and offer an opportunity to see common characteristics in a new way.
---
+++ scripts.js
@@ -0,0 +1,113 @@
+//Create the map
+var metMusData = [];
+var word;
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 25.730324, lng: 32.608309},
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+//---------------------------------------------------------------
+//Isolate a specific historic site by clicking name on the map -- 
+//and grab the name of the site to use in museum ajax call.
+//---------------------------------------------------------------
+
+      //store the original setContent-function
+      var fx = google.maps.InfoWindow.prototype.setContent;
+
+      //override the built-in setContent-method
+      google.maps.InfoWindow.prototype.setContent = function (content) {
+
+      //when argument is a node
+      if (content.querySelector) {
+
+      //search for the address
+      var name = content.querySelector('.gm-title');
+
+      if (name) {
+
+        word = name.textContent;
+      }
+    }
+
+    //run the original setContent-method
+    fx.apply(this, arguments);
+
+    //replace word spaces with underscores
+    word = word.split(' ').join('_');
+
+    console.log(word);
+
+//---------------------------
+//AJAX CALL TO MET MUSEUM API
+//---------------------------
+
+searchMetMuseum(word);
+
+function searchMetMuseum(word){
+  console.log(word);
+
+  var metMusURL = 'http://scrapi.org/search/' + word;
+
+  $.ajax({
+    url: metMusURL,
+    type: 'GET',
+    dataType: 'json',
+    error: function(data){
+      
+    },
+    success: function(data){
+
+      metMusData = data;
+
+      //Make certain we got the data
+      console.log(metMusData);
+
+      createMetHTML();
+
+//-----------------------
+//PUT MET RESULTS ON PAGE
+//-----------------------
+
+      function createMetHTML() {
+
+        console.log(metMusData);
+        console.log(word);
+        word = word.split('_').join(' ');
+
+        var htmlCategoryString = '';
+         htmlCategoryString += '<div class="museumSearchInfo">' + 'Items from ' + '<b>' + word + '</b>' + ' in the Metropolitan Museum of Art (www.metmuseum.org)' + '</div>';
+
+      $('#museumSearchInfo').append(htmlCategoryString);
+
+        var htmlString = '';
+        var dataLength = metMusData.collection.items.length;
+
+        for (var i = 0; i < dataLength; i++){
+        htmlString += '<div class="container">';
+        // htmlString += '<img src=' + metMusData.collection.items[i].image_thumb + '/>';
+        htmlString += '<h4>' + '<a href = ' + metMusData.collection.items[i].website_href + '>' + metMusData.collection.items[i].title + '</a>';
+        htmlString += '<p>' + 'Accession Number: ' + metMusData.collection.items[i].accessionNumber + '</p>';
+        htmlString += '<p>' + 'Date: ' + metMusData.collection.items[i].dateText + '</p>';
+        htmlString += '<p>' + 'Medium: ' + metMusData.collection.items[i].medium + '</p>';
+        // htmlString += '<p>' + 'Gallery: ' + metMusData.collection.items[i].gallery + '</p>';
+        htmlString += '</div>';
+      }
+
+      $('#metMuseumResults').append(htmlString);
+    }
+  }
+}
+);
+    }
+  };
+}
+
+
+$(document).ready(function(){
+  console.log("We are ready!");
+  initMap();
+});
